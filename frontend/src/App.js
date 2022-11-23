44b24464ee5617c284554be0e79d79630626b751
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-11-23 13:48:51 -0500

    front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Link, Routes, Route} from 'react-router-dom';
+import Tutorial from "./components/Tutorial";
+import AddTutorial from "./components/AddTutorial";
+import TutorialsList from "./components/TutorialsList";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+            <nav className="navbar navbar-expand navbar-dark bg-dark">
+              <a href="/tutorials" className="navbar-brand">
+                bezKoder
+              </a>
+              <div className="navbar-nav mr-auto">
+                <li className="nav-item">
+                  <Link to={"/tutorials"} className="nav-link">
+                    Tutorials
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to={"/add"} className="nav-link">
+                    Add
+                  </Link>
+                </li>
+              </div>
+            </nav>
+
+            <div className="container mt-3">
+              <Routes>
+                <Route path="/" element={<TutorialsList/>} />
+                <Route path="/tutorials" element={<TutorialsList/>} />
+                <Route path="/add" element={<AddTutorial/>} />
+                <Route path="/tutorials/:id" element={<Tutorial/>} />
+              </Routes>
+            </div>
+          </div>
   );
 }
 
